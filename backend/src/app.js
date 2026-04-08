15143d1a96c72d7f6d0040d289428bb82db009f7
NUR MOHAMMAD <nurmohammad0605@gmail.com>
2026-04-08 21:23:44 +0600

    Add cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,12 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://your-vercel-app.vercel.app"],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
